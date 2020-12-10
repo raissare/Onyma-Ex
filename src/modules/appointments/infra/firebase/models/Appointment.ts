06cb3fa7c269eecffeb694c1edc516c373ae80a6
Lucas Yuri <lucasyuri56@hotmail.com>
2020-12-09 21:31:13 -0300

    feat: add update static method

--- src/modules/appointments/infra/firebase/models/Appointment.ts
+++ src/modules/appointments/infra/firebase/models/Appointment.ts
@@ -51,6 +51,34 @@ class Appointment {
     if (dateTime) this._dateTime = dateTime;
     if (state) this._state = state;
   }
+
+  static update(
+    appointment: Partial<Omit<IAppointmentDTO, 'id'>>,
+  ): Partial<Omit<IAppointmentDTO, 'id'>> {
+    let update_appointemnt = {};
+    if (appointment.address)
+      update_appointemnt = {
+        ...update_appointemnt,
+        address: appointment.address,
+      };
+    if (appointment.patientName)
+      update_appointemnt = {
+        ...update_appointemnt,
+        patientName: appointment.patientName,
+      };
+    if (appointment.dateTime)
+      update_appointemnt = {
+        ...update_appointemnt,
+        dateTime: appointment.dateTime,
+      };
+    if (appointment.state)
+      update_appointemnt = {
+        ...update_appointemnt,
+        state: appointment.state,
+      };
+
+    return update_appointemnt;
+  }
 }
 
 export default Appointment;
