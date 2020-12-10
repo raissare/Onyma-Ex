01b4912ea9852aa60bce40f5b320b44816cce502
Lucas Yuri <lucasyuri56@hotmail.com>
2020-12-09 21:55:14 -0300

    feat: create update appointment use case

---
+++ src/modules/appointments/useCases/UpdateAppointment/UpdateAppointmentUseCase.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import { inject, injectable } from 'tsyringe';
+
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import IAppointmentDTO from '@modules/appointments/dtos/IAppointmentDTO';
+import IUpdateAppointmentDTO from '@modules/appointments/dtos/IUpdataAppoinmentDTO';
+
+@injectable()
+class UpdateAppointmentUseCase {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentRepository,
+  ) {}
+
+  async execute(data: IUpdateAppointmentDTO): Promise<IAppointmentDTO> {
+    const appointment = await this.appointmentsRepository.update(data);
+
+    return appointment;
+  }
+}
+
+export default UpdateAppointmentUseCase;
