edcc3df8009b4ca50086455e93da3e3fa7577975
Lucas Yuri <lucasyuri56@hotmail.com>
2020-12-09 22:48:46 -0300

    feat: use appointments routes at server

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,14 @@
+import 'reflect-metadata';
 import 'dotenv/config';
 
 import express from 'express';
 
 import '@shared/container';
+import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 export default app;
